eccd3eca95a1e5f954ac0f86e5d2c62d9968eba5
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-04-11 13:46:44 +0100

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,98 +1,249 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { ethers, utils } from "ethers";
 import abi from "./contracts/Bank.json";
 
 function App() {
   const [isWalletConnected, setIsWalletConnected] = useState(false);
   const [isBankerOwner, setIsBankerOwner] = useState(false);
-  const [inputValue, setInputValue] = useState({ withdraw: "", deposit: "", bankName: "" });
+  const [inputValue, setInputValue] = useState({
+    withdraw: "",
+    deposit: "",
+    bankName: "",
+  });
   const [bankOwnerAddress, setBankOwnerAddress] = useState(null);
   const [customerTotalBalance, setCustomerTotalBalance] = useState(null);
   const [currentBankName, setCurrentBankName] = useState(null);
   const [customerAddress, setCustomerAddress] = useState(null);
   const [error, setError] = useState(null);
 
-  const contractAddress = 'YOUR_CONTRACT_ADDRESS';
+  const contractAddress = "0x3c188Ee7505dAD7b104BDBe16F0BbE1C27AB4F51";
   const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const account = accounts[0];
+        setIsWalletConnected(true);
+        setCustomerAddress(account);
+        console.log("Account Created", account);
+      } else {
+        setError("Please install a MetaMask wallet to use our bank.");
+        console.log("No Metamask detected");
+      }
       //your code here
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const getBankName = async () => {
     try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const bankContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let bankName = await bankContract.bankName();
+        bankName = utils.parseBytes32String(bankName);
+
+        setCurrentBankName(bankName.toString());
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        setError("Please install a MetaMask wallet to use our bank.");
+      }
       //your code here
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const setBankNameHandler = async (event) => {
     event.preventDefault();
     try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const bankContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const txn = await bankContract.setBankName(
+          utils.formatBytes32String(inputValue.bankName)
+        );
+        console.log("Setting Bank Name...");
+
+        await txn.wait();
+        console.log("Bank Name Changed", txn.hash);
+        await getBankName();
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        setError("Please install a MetaMask wallet to use our bank.");
+      }
       //your code here
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const getbankOwnerHandler = async () => {
     try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const bankContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let owner = await bankContract.bankOwner();
+        setBankOwnerAddress(owner);
+
+        const [account] = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        if (owner.toLowerCase() === account.toLowerCase()) {
+          setIsBankerOwner(true);
+        }
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        setError("Please install a MetaMask wallet to use our bank.");
+      }
       //your code here
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const customerBalanceHanlder = async () => {
     try {
-      //your code here
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const bankContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        let balance = await bankContract.getCustomerBalance();
+        setCustomerTotalBalance(utils.formatEther(balance));
+
+        console.log("Retrieved balance...", balance);
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+
+        setError("Please install a MetaMask wallet to use our bank.");
+      }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const handleInputChange = (event) => {
-    setInputValue(prevFormData => ({ ...prevFormData, [event.target.name]: event.target.value }));
-  }
+    setInputValue((prevFormData) => ({
+      ...prevFormData,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   const deposityMoneyHandler = async (event) => {
     try {
-      //your code here
+      event.preventDefault();
+
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const bankContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        const txn = await bankContract.depositMoney({
+          value: ethers.utils.parseEther(inputValue.deposit),
+        });
+        console.log("Deposting money...");
+
+        await txn.wait();
+
+        console.log("Deposited money...done", txn.hash);
+
+        customerBalanceHanlder();
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+
+        setError("Please install a MetaMask wallet to use our bank.");
+      }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const withDrawMoneyHandler = async (event) => {
     try {
-      //your code here
+      event.preventDefault();
+
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const bankContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        let myAddress = await signer.getAddress();
+
+        console.log("provider signer...", myAddress);
+
+        const txn = await bankContract.withDrawMoney(
+          myAddress,
+          ethers.utils.parseEther(inputValue.withdraw)
+        );
+        console.log("Withdrawing money...");
+
+        await txn.wait();
+
+        console.log("Money with drew...done", txn.hash);
+
+        customerBalanceHanlder();
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+
+        setError("Please install a MetaMask wallet to use our bank.");
+      }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
     getBankName();
     getbankOwnerHandler();
-    customerBalanceHanlder()
-  }, [isWalletConnected])
+    customerBalanceHanlder();
+  }, [isWalletConnected]);
 
   return (
     <main className="main-container">
-      <h2 className="headline"><span className="headline-gradient">Bank Contract Project</span> 💰</h2>
+      <h2 className="headline">
+        <span className="headline-gradient">Bank Contract Project</span> 💰
+      </h2>
       <section className="customer-section px-10 pt-5 pb-10">
         {error && <p className="text-2xl text-red-700">{error}</p>}
         <div className="mt-5">
-          {currentBankName === "" && isBankerOwner ?
-            <p>"Setup the name of your bank." </p> :
+          {currentBankName === "" && isBankerOwner ? (
+            <p>"Setup the name of your bank." </p>
+          ) : (
             <p className="text-3xl font-bold">{currentBankName}</p>
-          }
+          )}
         </div>
         <div className="mt-7 mb-9">
           <form className="form-style">
@@ -104,9 +255,9 @@ function App() {
               placeholder="0.0000 ETH"
               value={inputValue.deposit}
             />
-            <button
-              className="btn-purple"
-              onClick={deposityMoneyHandler}>Deposit Money In ETH</button>
+            <button className="btn-purple" onClick={deposityMoneyHandler}>
+              Deposit Money In ETH
+            </button>
           </form>
         </div>
         <div className="mt-10 mb-10">
@@ -119,50 +270,57 @@ function App() {
               placeholder="0.0000 ETH"
               value={inputValue.withdraw}
             />
-            <button
-              className="btn-purple"
-              onClick={withDrawMoneyHandler}>
+            <button className="btn-purple" onClick={withDrawMoneyHandler}>
               Withdraw Money In ETH
             </button>
           </form>
         </div>
         <div className="mt-5">
-          <p><span className="font-bold">Customer Balance: </span>{customerTotalBalance}</p>
+          <p>
+            <span className="font-bold">Customer Balance: </span>
+            {customerTotalBalance}
+          </p>
         </div>
         <div className="mt-5">
-          <p><span className="font-bold">Bank Owner Address: </span>{bankOwnerAddress}</p>
+          <p>
+            <span className="font-bold">Bank Owner Address: </span>
+            {bankOwnerAddress}
+          </p>
         </div>
         <div className="mt-5">
-          {isWalletConnected && <p><span className="font-bold">Your Wallet Address: </span>{customerAddress}</p>}
+          {isWalletConnected && (
+            <p>
+              <span className="font-bold">Your Wallet Address: </span>
+              {customerAddress}
+            </p>
+          )}
           <button className="btn-connect" onClick={checkIfWalletIsConnected}>
             {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
           </button>
         </div>
       </section>
-      {
-        isBankerOwner && (
-          <section className="bank-owner-section">
-            <h2 className="text-xl border-b-2 border-indigo-500 px-10 py-4 font-bold">Bank Admin Panel</h2>
-            <div className="p-10">
-              <form className="form-style">
-                <input
-                  type="text"
-                  className="input-style"
-                  onChange={handleInputChange}
-                  name="bankName"
-                  placeholder="Enter a Name for Your Bank"
-                  value={inputValue.bankName}
-                />
-                <button
-                  className="btn-grey"
-                  onClick={setBankNameHandler}>
-                  Set Bank Name
-                </button>
-              </form>
-            </div>
-          </section>
-        )
-      }
+      {isBankerOwner && (
+        <section className="bank-owner-section">
+          <h2 className="text-xl border-b-2 border-indigo-500 px-10 py-4 font-bold">
+            Bank Admin Panel
+          </h2>
+          <div className="p-10">
+            <form className="form-style">
+              <input
+                type="text"
+                className="input-style"
+                onChange={handleInputChange}
+                name="bankName"
+                placeholder="Enter a Name for Your Bank"
+                value={inputValue.bankName}
+              />
+              <button className="btn-grey" onClick={setBankNameHandler}>
+                Set Bank Name
+              </button>
+            </form>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
